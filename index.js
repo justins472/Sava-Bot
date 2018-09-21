0ed0402e5604db96a18c1ae60af87f8427a5cdd3
justins472 <40777462+justins472@users.noreply.github.com>
2018-09-20 20:31:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Const Discord = require("discord.js");
+const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 const TOKEN = "NDkyNDg3NDgxODQ2MTM2ODQ1.DoXJiw.dhBRa3mclK2Ho2gR3U0jbuu3QzI"
 
