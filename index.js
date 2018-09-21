26412a7817d28bd7045eb85cb7acf5fc12a0867c
justins472 <40777462+justins472@users.noreply.github.com>
2018-09-20 20:23:14 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+Const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+const TOKEN = "NDkyNDg3NDgxODQ2MTM2ODQ1.DoXJiw.dhBRa3mclK2Ho2gR3U0jbuu3QzI"
+
+bot.login(TOKEN)
